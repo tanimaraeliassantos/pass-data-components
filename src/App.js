8af1926134cdaecf974743486bf9d2211c094f34
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-04 14:33:07 +1100

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Body from "./Body";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState("Joe");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header user={user} />
+      <Body user={user} />
+    </main>
   );
 }
-
-export default App;
